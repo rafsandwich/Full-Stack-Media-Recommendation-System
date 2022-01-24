221bda05a8633f0c2cb11780c928e329a4abd9a3
rafsandwich <rafhall@hotmail.com>
2022-01-24 12:07:29 +0000

    anime reviews
    increased functionality, can be deleted updated + a review feed ordered most recent first

--- animeweb/reviews/urls.py
+++ animeweb/reviews/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from .views import ReviewListView, ReviewDetailView, ReviewCreateView, ReviewUpdateView, ReviewDeleteView, UserReviewListView
 
 urlpatterns = [
-path('', views.reviewsMain, name='reviews-index')
-
+    path('', ReviewListView.as_view(), name='reviews-index'),
+  # path('', views.reviewsMain, name='reviews-index' 
+    path('<int:pk>/', ReviewDetailView.as_view(), name='reviews-detail'),
+    path('new/', ReviewCreateView.as_view(), name='reviews-create'), #primary key for unique review pages
+    path('<int:pk>/update/', ReviewUpdateView.as_view(), name='reviews-update'),
+    path('<int:pk>/delete/', ReviewDeleteView.as_view(), name='reviews-delete'),
+    path('user/<str:username>/', UserReviewListView.as_view(), name='user-reviews'),
 ]
--- animeweb/reviews/views.py
+++ animeweb/reviews/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin #(loginrequiredmixin) redirect new review attempts not logged in to login page
+from django.contrib.auth.models import User
 from recommend.models import Review
 
 #legacy dummy data
@@ -24,3 +26,64 @@ def reviewsMain(request):
         'reviews': Review.objects.all()
     }
     return render(request, 'reviews.html', context)
+
+class ReviewListView(ListView):
+    model = Review
+    template_name = 'reviews.html' #<model> name _ <viewtype>
+    context_object_name = 'reviews'
+    ordering = ['-date_posted'] #ordering newest anime reviews first
+    paginate_by = 6
+
+class UserReviewListView(ListView):
+    model = Review
+    template_name = 'user_reviews.html' 
+    context_object_name = 'reviews'
+    paginate_by = 6
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username')) #kwargs query paramaters, gets username from url
+        return Review.objects.filter(author=user).order_by('-date_posted')
+
+class ReviewDetailView(DetailView):
+    model = Review
+    template_name = 'review_detail.html'
+
+class ReviewCreateView(LoginRequiredMixin, CreateView):
+    model = Review
+    fields = ['head', 'body']
+    template_name = 'review_form.html'
+    #success_url = home page eg if wanted redirect somewhere else
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user #set author of instance to current logged in user
+        return super().form_valid(form) #send author before validation of form
+
+class ReviewUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Review
+    fields = ['head', 'body']
+    template_name = 'review_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form) 
+
+    #protect reviews from being updated by bad users
+    def test_func(self):
+        review = self.get_object() #get review trying to be updated
+        #if logged in user is the same as author of review in question
+        if self.request.user == review.author:
+            return True
+        else:
+            return False
+
+class ReviewDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Review
+    template_name = 'review_confirm_delete.html'
+    success_url='/reviews'
+
+    def test_func(self):
+        review = self.get_object()
+        if self.request.user == review.author:
+            return True
+        else:
+            return False
