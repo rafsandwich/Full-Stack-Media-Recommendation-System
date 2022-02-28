ab5f135c2c20a718f65ced9e0b18491c3671433c
rafsandwich <rafhall@hotmail.com>
2022-02-28 18:15:14 +0000

    UserList model correction

--- animeweb/users/signals.py
+++ animeweb/users/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save #signal fired after object saved
 from django.contrib.auth.models import User #sender, sends the signal
 from django.dispatch import receiver 
-from .models import Profile
+from .models import Profile, UserList
 
 #when a user is saved, then send this signal received by createProfile function
 @receiver(post_save, sender=User)
@@ -10,7 +10,9 @@ def createProfile(sender, instance, created, **kwargs):
     if created:
         #create profile object with user = instance of user that was created
         Profile.objects.create(user=instance)
+        UserList.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def saveProfile(sender, instance, **kwargs):
     instance.profile.save()
+    instance.userlist.save()
